a82637fcb400379687d09b23953e5d8443f9ce12
projet224solutions <projet224solutions@gmail.com>
2026-04-13 02:12:49 +0200

    fix: align service buttons as 2x2 grid on all screen sizes

--- src/components/home/NearbyServicesSection.tsx
+++ src/components/home/NearbyServicesSection.tsx
@@ -102,8 +102,8 @@ export function NearbyServicesSection({
         </div>
       </div>
 
-      {/* Services Grid - Landscape Layout */}
-      <div className="grid grid-cols-2 sm:grid-cols-4 gap-3">
+      {/* Services Grid - 2x2 Layout */}
+      <div className="grid grid-cols-2 gap-3">
         {services.map((service, index) => (
           <div
             key={service.id}
